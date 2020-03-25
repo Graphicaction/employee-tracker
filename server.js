b8ae5fb900dc1095681fe9747180697822b02fce
Rashmi Sharma <rksharma2679@gmail.com>
2020-03-25 15:24:49 -0400

    Restructuring files and folders

--- server.js
+++ server.js
@@ -1,29 +1,10 @@
-const mysql = require("mysql");
 const inquirer = require("inquirer");
 const department = require("./lib/department");
 const role = require("./lib/role");
 const employee = require("./lib/employee");
+const connection =require("./config/connection");
 
-// create the connection object
-const connection = mysql.createConnection({
-    host: "localhost",
-
-    // Your port; if not 3306
-    port: 3306,
-    
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "rashmi9876",
-    database: "employee_db"
-});
-
-connection.connect(function(err) {
-  if (err) throw err;
-  runSearch();
-});
-
+runSearch();
 //Starting search
 function runSearch() {
     inquirer
